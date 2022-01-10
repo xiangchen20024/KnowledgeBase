0de98a7b9075014ebb698f8e37da05c7e064fa07
xiangchen20024 <97435908+xiangchen20024@users.noreply.github.com>
2022-01-10 17:10:49 +0800

    Add back end structure for knowledge base
    Add back end structure for knowledge base

---
+++ knowledgeBase/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace knowledgeBase.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
